1b2fcb64760008a3d2f4e24592bcbb142275a916
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2020-11-13 14:14:57 -0500

    chore(GlButton): add label story

--- src/components/base/button/button.stories.js
+++ src/components/base/button/button.stories.js
@@ -271,4 +271,18 @@ documentedStoriesOf('base|button', readme)
         </gl-button>
       </div>
     `,
+  }))
+  .add('label button', () => ({
+    components,
+    template: `
+      <div>
+        <gl-button label>Label</gl-button>
+        <gl-button size="small" label>Label</gl-button>
+        <gl-button-group>
+          <gl-button label class="gl-font-monospace">b29cc44d</gl-button>
+          <gl-button icon="duplicate" />
+          <gl-button icon="folder-open" />
+        </gl-button-group>
+      </div>
+    `,
   }));
