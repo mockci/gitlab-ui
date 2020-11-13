bfc9d0381e13f7275bf344ab1dac6deb1ea4f320
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2020-11-13 16:11:30 -0500

    chore(GlButton): apply aria-labelledby to story and example

--- src/components/base/button/button.stories.js
+++ src/components/base/button/button.stories.js
@@ -279,8 +279,10 @@ documentedStoriesOf('base|button', readme)
         <gl-button label>Label</gl-button>
         <gl-button size="small" label>Label</gl-button>
         <gl-button-group>
-          <gl-button label class="gl-font-monospace">b29cc44d</gl-button>
-          <gl-button icon="duplicate" />
+          <gl-button id="commit-sha-label" class="gl-font-monospace" label>
+            b29cc44d
+          </gl-button>
+          <gl-button aria-labelledby="commit-sha-label" icon="duplicate" />
           <gl-button icon="folder-open" />
         </gl-button-group>
       </div>
